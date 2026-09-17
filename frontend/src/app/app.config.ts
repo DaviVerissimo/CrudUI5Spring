8ce3cf4a0d94acd72b72a56723501ea91f6153ef
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2026-09-17 12:37:21 -0300

    add dependências front

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+//para apps stadalone (angula moderno)
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
