f60e86a895e51f57ccb0a2807ee7a2808ae87de6
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2026-09-22 16:26:59 -0300

    Create CustomUserDetailsService.java

---
+++ backend/api/src/main/java/com/example/api/security/CustomUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.example.api.security;
+
+import com.example.api.repository.UserRepository;
+import com.example.api.model.User;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public CustomUserDetailsService(
+            UserRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "Usuário não encontrado"
+                        )
+                );
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(
+                        new SimpleGrantedAuthority(
+                                "ROLE_" + user.getRole()
+                        )
+                )
+        );
+    }
+}
